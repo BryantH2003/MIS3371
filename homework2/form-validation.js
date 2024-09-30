8fd7967730e6d298302d54154624d21cf86a88b8
BryantH2003 <88125614+BryantH2003@users.noreply.github.com>
2024-09-30 18:04:53 -0500

    Initial validation for first name

--- homework2/form-validation.js
+++ homework2/form-validation.js
@@ -1,6 +1,17 @@
+// Validation for First Name field
 function validateFirstName () {
     let firstname = document.forms["patient-form"]["firstname"].value;
-
+    
+    if (firstname.length < 1) {
+        document.getElementById("firstNameValidator").innerHTML = '<p class="error-message">This field is required!</p>';
+        document.getElementById("firstName").style.backgroundColor = "#ffc8c8"
+    } else if (firstname.length > 30) {
+        document.getElementById("firstNameValidator").innerHTML = '<p class="error-message">Too many characters!</p>';
+        document.getElementById("firstName").style.backgroundColor = "#ffc8c8"
+    } else {
+        document.getElementById("firstNameValidator").innerHTML = '';
+        document.getElementById("firstName").style.backgroundColor = ""
+    }
     
 }
 
